5f1de3b8119c391b8260def54d68f5b2200e1136
LS554 <103077889+LS554@users.noreply.github.com>
2025-06-07 00:54:19 -0400

    Add files via upload

---
+++ dirtool/dirtool.nim
@@ -0,0 +1,23 @@
+import os
+import strformat
+
+proc mkstruct(dir: string) =
+    if not dirExists(dir):
+        var folder:string = dir & ".AppDir"
+        var usr:string = &"{folder}/usr"
+        var bin:string = &"{folder}/usr/bin"
+        var AppRun:string = &"{folder}/" & "AppRun"
+        createDir(folder)
+        createDir(usr)
+        createDir(bin)
+        writeFile(AppRun, "")
+    else:
+        echo &"Error, directory {dir} exists. Quitting." 
+        quit(1)
+        
+
+if paramCount() < 1:
+    echo "Usage: dirtool 'app name'"
+elif 1 == paramCount():
+    var app_name:string = paramStr(1)
+    mkstruct(app_name)
